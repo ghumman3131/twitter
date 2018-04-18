4cd872f0d3d3ae0161c05e0b796d2d769c00c6c0
ghumman3131 <charanghumman@gmail.com>
2018-04-18 08:35:09 +0530

    first commit

---
+++ app/src/main/java/com/inception/twitter_test/MainActivity.java
@@ -0,0 +1,61 @@
+package com.inception.twitter_test;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.DefaultLogger;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterConfig;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+import com.twitter.sdk.android.tweetui.UserTimeline;
+
+public class MainActivity extends AppCompatActivity {
+
+    TwitterLoginButton loginButton ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TwitterConfig config = new TwitterConfig.Builder(this)
+                .logger(new DefaultLogger(Log.DEBUG))
+                .twitterAuthConfig(new TwitterAuthConfig("BtAHqI5lrENzN0aIPcVV8wCzJ", "A0cBGq5lzzgUl8KNftVz0hNlmuJHZQKA0RWe4xTfPOoim1el8c"))
+                .debug(true)
+                .build();
+        Twitter.initialize(config);
+
+        setContentView(R.layout.activity_main);
+
+       loginButton = (TwitterLoginButton) findViewById(R.id.login_button);
+        loginButton.setCallback(new Callback<TwitterSession>() {
+            @Override
+            public void success(Result<TwitterSession> result) {
+                // Do something with result, which provides a TwitterSession for making API calls
+            }
+
+            @Override
+            public void failure(TwitterException exception) {
+                // Do something on failure
+            }
+        });
+
+
+        UserTimeline userTimeline = new UserTimeline.Builder().screenName("twitterdev").build();
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Pass the activity result to the login button.
+        loginButton.onActivityResult(requestCode, resultCode, data);
+    }
+}
