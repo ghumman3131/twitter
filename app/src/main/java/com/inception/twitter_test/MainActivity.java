9c53ca712ea5354d5f3a4d066d1fcbf086aa38b1
ghumman3131 <charanghumman@gmail.com>
2018-04-30 06:15:27 +0530

    timeline

--- app/src/main/java/com/inception/twitter_test/MainActivity.java
+++ app/src/main/java/com/inception/twitter_test/MainActivity.java
@@ -1,24 +1,40 @@
 package com.inception.twitter_test;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
+import com.inception.twitter_test.fragment.MakePostFragment;
+import com.inception.twitter_test.fragment.SearchTimeLineFragment;
+import com.inception.twitter_test.fragment.UserTimeLineFragment;
 import com.twitter.sdk.android.core.Callback;
 import com.twitter.sdk.android.core.DefaultLogger;
 import com.twitter.sdk.android.core.Result;
 import com.twitter.sdk.android.core.Twitter;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
 import com.twitter.sdk.android.core.TwitterConfig;
+import com.twitter.sdk.android.core.TwitterCore;
 import com.twitter.sdk.android.core.TwitterException;
 import com.twitter.sdk.android.core.TwitterSession;
 import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+import com.twitter.sdk.android.tweetcomposer.TweetComposer;
+import com.twitter.sdk.android.tweetui.SearchTimeline;
+import com.twitter.sdk.android.tweetui.TweetTimelineRecyclerViewAdapter;
 import com.twitter.sdk.android.tweetui.UserTimeline;
 
 public class MainActivity extends AppCompatActivity {
 
-    TwitterLoginButton loginButton ;
+
+    FragmentManager fm = getSupportFragmentManager();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,29 +49,70 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-       loginButton = (TwitterLoginButton) findViewById(R.id.login_button);
-        loginButton.setCallback(new Callback<TwitterSession>() {
-            @Override
-            public void success(Result<TwitterSession> result) {
-                // Do something with result, which provides a TwitterSession for making API calls
-            }
-
-            @Override
-            public void failure(TwitterException exception) {
-                // Do something on failure
-            }
-        });
 
 
-        UserTimeline userTimeline = new UserTimeline.Builder().screenName("twitterdev").build();
+
+
     }
 
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+    protected void onResume() {
+        super.onResume();
 
-        // Pass the activity result to the login button.
-        loginButton.onActivityResult(requestCode, resultCode, data);
+
+        final TwitterSession session = TwitterCore.getInstance().getSessionManager()
+                .getActiveSession();
+
+        if(session != null) {
+
+
+            FragmentTransaction ft = fm.beginTransaction();
+
+            ft.replace(R.id.main_frame, new UserTimeLineFragment()).commit();
+
+        }
+
+        else {
+
+            startActivity(new Intent(MainActivity.this , TwitterLoginActivity.class));
+        }
+
+
     }
+
+    public void open_timeline(View view) {
+
+
+        FragmentTransaction ft = fm.beginTransaction();
+
+        ft.replace(R.id.main_frame , new UserTimeLineFragment()).commit();
+
+    }
+
+    public void search_timeline(View view) {
+
+        FragmentTransaction ft = fm.beginTransaction();
+
+        ft.replace(R.id.main_frame , new SearchTimeLineFragment()).commit();
+
+    }
+
+    public void post_timeline(View view) {
+
+        FragmentTransaction ft = fm.beginTransaction();
+
+        ft.replace(R.id.main_frame , new MakePostFragment()).commit();
+
+    }
+
+    public void log_out_twitter(View view) {
+
+        TwitterCore.getInstance().getSessionManager()
+                .clearActiveSession();
+
+        finish();
+    }
+
+
 }
---
+++ app/src/main/java/com/inception/twitter_test/fragment/MakePostFragment.java
@@ -0,0 +1,62 @@
+package com.inception.twitter_test.fragment;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.inception.twitter_test.R;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterCore;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+import com.twitter.sdk.android.tweetcomposer.TweetComposer;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MakePostFragment extends Fragment {
+
+
+    public MakePostFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_make_post, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+        final TwitterSession session = TwitterCore.getInstance().getSessionManager()
+                .getActiveSession();
+
+
+        if (session != null) {
+
+
+            TweetComposer.Builder builder = new TweetComposer.Builder(getActivity())
+                    .text("just setting up my Twitter Kit.");
+            builder.show();
+
+        } else {
+
+
+        }
+    }
+
+
+}
